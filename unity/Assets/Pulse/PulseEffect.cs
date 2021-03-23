425489c509785ae7a807e4138c4f372fac07f631
Mike Hayes <mghayes93@gmail.com>
2021-03-22 23:36:21 -0700

    working pulses

--- unity/Assets/Pulse/PulseEffect.cs
+++ unity/Assets/Pulse/PulseEffect.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PulseEffect : MonoBehaviour
 {
 	public Transform PulseOrigin;
-	public Material PulseMat;
+	public Material[] PulseMat;
 	public float PulseDistance;
 	public float PulseSpeed;
 	public Camera _Camera;
@@ -14,6 +14,36 @@ public class PulseEffect : MonoBehaviour
 	public GameObject Player;
 
 	bool _scanning;
+
+	public List<Echolocation> echos = new List<Echolocation>();
+
+	public class Echolocation
+	{
+		public Material EchoPulseMat;
+		public Transform EchoPulseOrigin;
+		public float EchoPulseDistance;
+		public float EchoPulseSpeed;
+		public RenderTexture EchoRenderTexture;
+	}
+
+	int test = 0;
+
+	void Start() 
+	{
+		//AddPulse(PulseMat[0], PulseOrigin, PulseSpeed);
+	}
+
+	private Echolocation AddPulse(Material _pulseMat, Transform _pulseOrigin, float _pulseSpeed)
+	{
+		var e = new Echolocation{
+				EchoPulseMat = _pulseMat,
+				EchoPulseDistance = 0.0f,
+				EchoPulseOrigin = _pulseOrigin,
+				EchoPulseSpeed = _pulseSpeed,
+				EchoRenderTexture = RenderTexture.GetTemporary(1024,1024)};
+		echos.Add(e);
+		return e;
+	}
 
 	void OnEnable() 
 	{
@@ -25,7 +55,12 @@ public class PulseEffect : MonoBehaviour
 		if (_scanning)
 		{
 			// Do cool shit here
-			PulseDistance += Time.deltaTime * PulseSpeed;
+			for(int i = 0; i < echos.Count; i++)
+			{
+				echos[i].EchoPulseDistance += Time.deltaTime * echos[i].EchoPulseSpeed;
+			}
+
+			//PulseDistance += Time.deltaTime * PulseSpeed;
 
 			// probably not efficient to check every object? Not sure?
 			foreach(var s in ScannableObjects)
@@ -42,8 +77,10 @@ public class PulseEffect : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.P))
 		{
 			_scanning = true;
-			PulseDistance = 0.0f;
-			PulseOrigin.position = Player.transform.position;
+			var e = AddPulse(PulseMat[test], PulseOrigin, PulseSpeed);
+			e.EchoPulseDistance = 0;
+			e.EchoPulseOrigin.position = Player.transform.position;
+			test = (test + 1) % 2;
 		}
 
 		if (Input.GetMouseButtonDown(0))
@@ -54,8 +91,9 @@ public class PulseEffect : MonoBehaviour
 			if (Physics.Raycast(ray, out hit))
 			{
 				_scanning = true;
-				PulseDistance = 0;
-				PulseOrigin.position = hit.point;
+				var e = AddPulse(PulseMat[0], PulseOrigin, PulseSpeed);
+				e.EchoPulseDistance = 0;
+				e.EchoPulseOrigin.position = hit.point;
 			}
 		}
 	}
@@ -63,70 +101,92 @@ public class PulseEffect : MonoBehaviour
 	[ImageEffectOpaque]
 	void OnRenderImage(RenderTexture src, RenderTexture dst)
 	{
-		PulseMat.SetVector("_WorldSpacePulsePos", PulseOrigin.position);
-		PulseMat.SetFloat("_PulseDistance", PulseDistance);
-		RaycastCornerBlit(src, dst, PulseMat);
+		if (echos.Count == 0)
+		{
+			Graphics.Blit(src,dst);
+		}
+		else
+		{
+			RaycastCornerBlit(src, dst, echos);
+		}
 	}
 
-	private void RaycastCornerBlit(RenderTexture src, RenderTexture dest, Material pulseMat)
+	private void RaycastCornerBlit(RenderTexture src, RenderTexture dest, List<Echolocation> pulses)
 	{
-		// Compute Frustum Corners
-		float camFar = _Camera.farClipPlane;
-		float camFov = _Camera.fieldOfView;
-		float camAspect = _Camera.aspect;
+		bool swapped = false;
+		foreach(var pulse in pulses)
+		{
+			pulse.EchoPulseMat.SetVector("_WorldSpacePulsePos", pulse.EchoPulseOrigin.position);
+			pulse.EchoPulseMat.SetFloat("_PulseDistance", pulse.EchoPulseDistance);
+			// Compute Frustum Corners
+			float camFar = _Camera.farClipPlane;
+			float camFov = _Camera.fieldOfView;
+			float camAspect = _Camera.aspect;
 
-		float fovWHalf = camFov * 0.5f;
+			float fovWHalf = camFov * 0.5f;
 
-		Vector3 toRight = _Camera.transform.right * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
-		Vector3 toTop = _Camera.transform.up * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+			Vector3 toRight = _Camera.transform.right * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+			Vector3 toTop = _Camera.transform.up * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
 
-		Vector3 topLeft = (_Camera.transform.forward - toRight + toTop);
-		float camScale = topLeft.magnitude * camFar;
+			Vector3 topLeft = (_Camera.transform.forward - toRight + toTop);
+			float camScale = topLeft.magnitude * camFar;
 
-		topLeft.Normalize();
-		topLeft *= camScale;
+			topLeft.Normalize();
+			topLeft *= camScale;
 
-		Vector3 topRight = (_Camera.transform.forward + toRight + toTop);
-		topRight.Normalize();
-		topRight *= camScale;
+			Vector3 topRight = (_Camera.transform.forward + toRight + toTop);
+			topRight.Normalize();
+			topRight *= camScale;
 
-		Vector3 bottomRight = (_Camera.transform.forward + toRight - toTop);
-		bottomRight.Normalize();
-		bottomRight *= camScale;
+			Vector3 bottomRight = (_Camera.transform.forward + toRight - toTop);
+			bottomRight.Normalize();
+			bottomRight *= camScale;
 
-		Vector3 bottomLeft = (_Camera.transform.forward - toRight - toTop);
-		bottomLeft.Normalize();
-		bottomLeft *= camScale;
+			Vector3 bottomLeft = (_Camera.transform.forward - toRight - toTop);
+			bottomLeft.Normalize();
+			bottomLeft *= camScale;
 
-		// Custom Blit, encoding Frustum Corners as additional Texture Coordinates
-		RenderTexture.active = dest;
+			// Custom Blit, encoding Frustum Corners as additional Texture Coordinates
+			RenderTexture.active = dest;
 
-		pulseMat.SetTexture("_MainTex", src);
+			pulse.EchoPulseMat.SetTexture("_MainTex", src);
 
-		GL.PushMatrix();
-		GL.LoadOrtho();
+			GL.PushMatrix();
+			GL.LoadOrtho();
 
-		pulseMat.SetPass(0);
+			pulse.EchoPulseMat.SetPass(0);
 
-		GL.Begin(GL.QUADS);
+			GL.Begin(GL.QUADS);
 
-		GL.MultiTexCoord2(0, 0.0f, 0.0f);
-		GL.MultiTexCoord(1, bottomLeft);
-		GL.Vertex3(0.0f, 0.0f, 0.0f);
+			GL.MultiTexCoord2(0, 0.0f, 0.0f);
+			GL.MultiTexCoord(1, bottomLeft);
+			GL.Vertex3(0.0f, 0.0f, 0.0f);
 
-		GL.MultiTexCoord2(0, 1.0f, 0.0f);
-		GL.MultiTexCoord(1, bottomRight);
-		GL.Vertex3(1.0f, 0.0f, 0.0f);
+			GL.MultiTexCoord2(0, 1.0f, 0.0f);
+			GL.MultiTexCoord(1, bottomRight);
+			GL.Vertex3(1.0f, 0.0f, 0.0f);
 
-		GL.MultiTexCoord2(0, 1.0f, 1.0f);
-		GL.MultiTexCoord(1, topRight);
-		GL.Vertex3(1.0f, 1.0f, 0.0f);
+			GL.MultiTexCoord2(0, 1.0f, 1.0f);
+			GL.MultiTexCoord(1, topRight);
+			GL.Vertex3(1.0f, 1.0f, 0.0f);
 
-		GL.MultiTexCoord2(0, 0.0f, 1.0f);
-		GL.MultiTexCoord(1, topLeft);
-		GL.Vertex3(0.0f, 1.0f, 0.0f);
+			GL.MultiTexCoord2(0, 0.0f, 1.0f);
+			GL.MultiTexCoord(1, topLeft);
+			GL.Vertex3(0.0f, 1.0f, 0.0f);
 
-		GL.End();
-		GL.PopMatrix();
+			GL.End();
+			GL.PopMatrix();
+
+			var temp = src;
+			src = dest;
+			dest = temp;
+
+			swapped = !swapped;
+		}
+
+		if (!swapped)
+		{
+			Graphics.Blit(src, dest);
+		}
 	}
 }
